8d6720a71b1401f69bfd33d19ab248d4a23be45a
Martin Wagner <22003632+Waldleufer@users.noreply.github.com>
2023-06-20 12:12:46 +0200

    exercise 3
    - Write data into a SQLite database called “cars.sqlite”, in the table “cars”
    - Pick suitable encoding (Make sure to preserve the german special letters like “ü” or “ä”)
    - Reshape data structure
      - Ignore the first 6 lines and last 4 lines as metadata
      - Keep only the following columns, rename them to the new name given here (M-BU contain summary data)
        Column A: date
        Column B: CIN
        Column C: name
        Column M: petrol
        Column W: diesel
        Column AG: gas
        Column AQ: electro
        Column BA: hybrid
        Column BK: plugInHybrid
        Column BU: others
        Drop all other columns
    - Validate data
        - date/name are strings, no need to validate date
        - CINs are Community Identification Numbers, must be strings with 5 characters and can have a leading 0
        - all other columns should be positive integers > 0
        - drop all rows that contain invalid values
        - Use fitting SQLite types (e.g., BIGINT, TEXT or FLOAT) for all columns

---
+++ exercises/exercise3.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import time
+import numpy as np
+from sqlalchemy import create_engine
+
+# Step 1: Read the CSV File
+# The encoding is set to 'iso-8859-1' to preserve German special characters
+# Skip the first 6 and last 4 rows since they contain metadata
+# Perform exponential back-off if unsucessful for 4 additional times after the initial call failed.
+max_retries = 5
+for attempt in range(max_retries):
+    try:
+        df = pd.read_csv('https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv',
+                         sep=";", skiprows=6, skipfooter=4, encoding='iso-8859-1', engine='python',
+                         dtype={'Unnamed: 1': str}) # Convert the type of the second column to string
+        break
+    except Exception as e:
+        if attempt < max_retries - 1:  # i.e. not on last attempt
+            wait_time = 2 ** attempt  # exponential back-off
+            print(f"Attempt {attempt + 1} failed, retrying in {wait_time} seconds...")
+            time.sleep(wait_time)
+        else:
+            raise Exception(f"Failed to fetch data after {max_retries} attempts. The last exception was: {str(e)}") from None
+# Step 2: Data Filtering and Renaming
+# Rename columns based on their index
+
+columns = df.columns.tolist()
+indices_to_rename = {
+    0: 'date',
+    1: 'CIN',
+    2: 'name',
+    12: 'petrol',
+    22: 'diesel',
+    32: 'gas',
+    41: 'electro',
+    52: 'hybrid',
+    61: 'plugInHybrid',
+    68: 'others'
+}
+for index, new_name in indices_to_rename.items():
+    if index < len(columns):
+        columns[index] = new_name
+df.columns = columns
+
+# Keep only required columns
+df = df[['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']]
+
+# Step 3: Data Validation
+# "-" should be replaced with NaN.
+df = df.replace('-', np.nan)
+
+# CINs must be strings with 5 characters
+df = df[df['CIN'].apply(lambda x: isinstance(x, str) and len(x) == 5)]
+
+# All other columns should be positive integers > 0
+for column in ['petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']:
+    df[column] = pd.to_numeric(df[column], errors='coerce')  # convert values to numeric, replace non-numeric values with NaN
+    df = df[df[column].apply(lambda x: np.isnan(x) or x > 0)]
+
+# Step 4: Write to SQLite Database
+engine = create_engine('sqlite:///cars.sqlite')
+df.to_sql('cars', engine, if_exists='replace', index=False)
