647a0a649417d6457dd44eb72103b8eb8b0d277d
Martin Wagner <22003632+Waldleufer@users.noreply.github.com>
2023-06-27 20:13:41 +0200

    #6 Refactoring

--- data/datagetter_test.py
+++ data/datagetter_test.py
@@ -5,17 +5,17 @@ from datagetter import execute_pipeline
 
 def test_execute_pipeline():
     # Delete the database if it exists
-    if os.path.exists("preprocessed-data.db"):
-        os.remove("preprocessed-data.db")
+    if os.path.exists("preprocessed-data.sqlite"):
+        os.remove("preprocessed-data.sqlite")
 
     # Run the pipeline
     execute_pipeline()
 
     # Verify that the database file was created
-    assert os.path.exists("preprocessed-data.db"), "Database file was not created"
+    assert os.path.exists("preprocessed-data.sqlite"), "Database file was not created"
 
     # Verify that the correct tables were created in the database
-    conn = sqlite3.connect('preprocessed-data.db')
+    conn = sqlite3.connect('preprocessed-data.sqlite')
     cursor = conn.cursor()
 
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
